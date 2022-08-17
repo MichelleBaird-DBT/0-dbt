2ff20909c7e67c739b54ca3df9b0e83819099d7a
Michelle Baird <michelle.baird@fishtownanalytics.com>
2022-08-17 13:41:25 +0000

    still a bit or work to do, but this is more or less what the project they'll start off with will look like.

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,38 @@
+with customer as (
+
+    select * from {{ ref('stg_customer') }}
+
+),
+nation as (
+
+    select * from {{ ref('stg_nations') }}
+),
+region as (
+
+    select * from {{ ref('stg_regions') }}
+
+),
+final as (
+    select 
+        customer.customer_key,
+        customer.customer_name,
+        {# nation.nation_key as nation_key, #}
+        nation.nation_name as nation,
+        {# region.region_key as region_key, #}
+        region.region_name as region,
+        customer.customer_phone,
+        customer.customer_acct_balance
+        -- new column
+    from
+        customer
+        inner join nation
+            on customer.nation_key = nation.nation_key
+        inner join region
+            on nation.region_key = region.region_key
+)
+select 
+    *
+from
+    final
+order by
+    customer_key
---
+++ models/staging/sample_data/stg_nations.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+  select * from {{ source('snowflake_sample_data', 'nation') }}
+
+),
+
+transformed as (
+
+  select 
+    n_nationkey as nation_key,
+    n_name as nation_name,
+    n_regionkey as region_key
+    
+
+
+  from source
+
+)
+
+select * from transformed
